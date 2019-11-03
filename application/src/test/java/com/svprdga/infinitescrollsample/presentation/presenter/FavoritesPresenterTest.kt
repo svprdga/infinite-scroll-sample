a4ef3d461e821b10c122494cb234bf36369b60a4
svprdga <svprdga@tutanota.com>
2019-11-03 13:31:58 +0100

    #6 Add FavoritesPresenterTest & BusTest

---
+++ application/src/test/java/com/svprdga/infinitescrollsample/presentation/presenter/FavoritesPresenterTest.kt
@@ -0,0 +1,192 @@
+package com.svprdga.infinitescrollsample.presentation.presenter
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.svprdga.infinitescrollsample.domain.Show
+import com.svprdga.infinitescrollsample.domain.ShowData
+import com.svprdga.infinitescrollsample.domain.exception.KoException
+import com.svprdga.infinitescrollsample.domain.usecase.ShowsUseCase
+import com.svprdga.infinitescrollsample.presentation.eventbus.FavoriteEvent
+import com.svprdga.infinitescrollsample.presentation.eventbus.FavoritesBus
+import com.svprdga.infinitescrollsample.presentation.presenter.view.IFavoritesView
+import com.svprdga.infinitescrollsample.util.Logger
+import de.bechte.junit.runners.context.HierarchicalContextRunner
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+private const val TOTAL_ITEMS = 5
+
+@RunWith(HierarchicalContextRunner::class)
+class FavoritesPresenterTest {
+
+    // *************************************** MOCK VARS *************************************** //
+
+    @Mock
+    lateinit var log: Logger
+    @Mock
+    lateinit var showsUseCase: ShowsUseCase
+    @Mock
+    lateinit var view: IFavoritesView
+
+    // ****************************************** VARS ***************************************** //
+
+    lateinit var presenter: FavoritesPresenter
+    var shows: ArrayList<Show> = arrayListOf()
+
+    val singleSuccess = object : Single<List<Show>>() {
+        override fun subscribeActual(observer: SingleObserver<in List<Show>>) {
+            observer.onSuccess(shows)
+        }
+    }
+
+    val singleSuccessEmpty = object : Single<List<Show>>() {
+        override fun subscribeActual(observer: SingleObserver<in List<Show>>) {
+            observer.onSuccess(arrayListOf())
+        }
+    }
+
+    val exception = KoException(400, null)
+    val singleError = object : Single<List<Show>>() {
+        override fun subscribeActual(observer: SingleObserver<in List<Show>>) {
+            observer.onError(exception)
+        }
+    }
+
+    val favoritesBus = FavoritesBus()
+
+    // ***************************************** SET UP **************************************** //
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        for (i in 0..TOTAL_ITEMS) {
+            shows.add(
+                Show(
+                    i, "name_$i", "overview_$i",
+                    100F - i.toFloat(), "path_$i", true
+                )
+            )
+        }
+
+        whenever(showsUseCase.findAllFavoritesAsync()).thenReturn(singleSuccess)
+
+        presenter = FavoritesPresenter(log, showsUseCase, favoritesBus)
+    }
+
+    // ***************************************** TESTS ***************************************** //
+
+    inner class `when calling bind()` {
+
+        @Test
+        fun `should fetch initial set of items`() {
+            whenever(showsUseCase.findAllFavoritesAsync()).thenReturn(singleSuccess)
+            presenter.bind(view)
+            verify(showsUseCase).findAllFavoritesAsync()
+        }
+
+        inner class `given success with an empty array`() {
+
+            @Before
+            fun setUp() {
+                whenever(showsUseCase.findAllFavoritesAsync()).thenReturn(singleSuccessEmpty)
+            }
+
+            @Test
+            fun `should show empty favorites layout in view`() {
+                presenter.bind(view)
+                verify(view).showEmptyFavoritesLayout()
+            }
+        }
+
+        inner class `given success with a list of Show`() {
+
+            @Before
+            fun setUp() {
+                presenter.bind(view)
+            }
+
+            @Test
+            fun `should hide empty favorites layout in view`() {
+                verify(view).hideEmptyFavoritesLayout()
+            }
+
+            @Test
+            fun `should display fetched shows in view`() {
+                verify(view).setFavorites(shows)
+            }
+        }
+
+        inner class `given error when fetching content`() {
+
+            @Before
+            fun setUp() {
+                whenever(showsUseCase.findAllFavoritesAsync()).thenReturn(singleError)
+                presenter.bind(view)
+            }
+
+            @Test
+            fun `should log error`() {
+                verify(log).error(any())
+            }
+        }
+    }
+
+    inner class `when receiving a FavoriteEvent` {
+
+        inner class `given a Show is no longer a favorite` {
+
+            lateinit var show: Show
+            val layoutPosition = 2
+
+            @Before
+            fun setUp() {
+                show = shows[0]
+
+                show.isFavorite = false
+                presenter.bind(view)
+                favoritesBus.setFavoriteEvent(FavoriteEvent(show, layoutPosition))
+            }
+
+            @Test
+            fun `should remove Show from the list in view`() {
+                verify(view).removeShowFromList(layoutPosition)
+            }
+
+        }
+
+        inner class `given last Show to be removed from favorites` {
+
+            lateinit var show: Show
+            val layoutPosition = 0
+
+            @Before
+            fun setUp() {
+                show = shows[0]
+
+                show.isFavorite = false
+                presenter.bind(view)
+                favoritesBus.setFavoriteEvent(FavoriteEvent(show, layoutPosition))
+            }
+
+            @Test
+            fun `should remove Show from the list in view`() {
+                verify(view).removeShowFromList(layoutPosition)
+            }
+
+            @Test
+            fun `should show empty favorites layout in view`() {
+                verify(view).hideEmptyFavoritesLayout()
+            }
+
+        }
+
+    }
+
+}
--- application/src/test/java/com/svprdga/infinitescrollsample/presentation/presenter/ListPresenterTest.kt
+++ application/src/test/java/com/svprdga/infinitescrollsample/presentation/presenter/ListPresenterTest.kt
@@ -1,159 +1,159 @@
 package com.svprdga.infinitescrollsample.presentation.presenter
-//
-//import com.nhaarman.mockitokotlin2.*
-//import com.svprdga.infinitescrollsample.domain.Show
-//import com.svprdga.infinitescrollsample.domain.ShowData
-//import com.svprdga.infinitescrollsample.domain.exception.KoException
-//import com.svprdga.infinitescrollsample.domain.repository.IShowRepository
-//import com.svprdga.infinitescrollsample.presentation.presenter.view.IListView
-//import com.svprdga.infinitescrollsample.util.Logger
-//import de.bechte.junit.runners.context.HierarchicalContextRunner
-//import io.reactivex.Single
-//import io.reactivex.SingleObserver
-//import org.junit.Before
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//import org.mockito.Mock
-//import org.mockito.MockitoAnnotations
-//
-//@RunWith(HierarchicalContextRunner::class)
-//class ListPresenterTest {
-//
-//    // *************************************** MOCK VARS *************************************** //
-//
-//    @Mock
-//    lateinit var log: Logger
-//    @Mock
-//    lateinit var showRepository: IShowRepository
-//    @Mock
-//    lateinit var view: IListView
-//    @Mock
-//    lateinit var showData: ShowData
-//
-//    // ****************************************** VARS ***************************************** //
-//
-//    lateinit var presenter: ListPresenter
-//    var shows: ArrayList<Show> = arrayListOf()
-//
-//    val singleSuccess = object : Single<ShowData>() {
-//        override fun subscribeActual(observer: SingleObserver<in ShowData>) {
-//            observer.onSuccess(showData)
-//        }
-//    }
-//
-//    val exception = KoException(400, null)
-//    val singleError = object : Single<ShowData>() {
-//        override fun subscribeActual(observer: SingleObserver<in ShowData>) {
-//            observer.onError(exception)
-//        }
-//    }
-//
-//    // ***************************************** SET UP **************************************** //
-//
-//    @Before
-//    fun setUp() {
-//        MockitoAnnotations.initMocks(this)
-//
-//        for (i in 1..10) {
-//            shows.add(
-//                Show(
-//                    i, "name_$i",
-//                    "overview_$i",
-//                    i.toFloat(),
-//                    "path_$1",
-//                    false
-//                )
-//            )
-//        }
-//
-//        whenever(showData.shows).thenReturn(shows)
-//
-//        presenter = ListPresenter(log, showRepository)
-//    }
-//
-//    // ***************************************** TESTS ***************************************** //
-//
-//    inner class `when calling bind()` {
-//
-//        @Before
-//        fun setUp() {
-//            whenever(showRepository.findPopularShows(1)).thenReturn(singleSuccess)
-//        }
-//
-//        @Test
-//        fun `should fetch initial set of items`() {
-//            presenter.bind(view)
-//            verify(showRepository).findPopularShows(1)
-//        }
-//
-//        inner class `given success when fetching content`() {
-//
-//            @Test
-//            fun `should display fetched items in view`() {
-//                presenter.bind(view)
-//                verify(view).appendShows(showData.shows)
-//            }
-//
-//        }
-//
-//        inner class `given error when fetching content`() {
-//
-//            @Before
-//            fun setUp() {
-//                whenever(showRepository.findPopularShows(1)).thenReturn(singleError)
-//                presenter.bind(view)
-//            }
-//
-//            @Test
-//            fun `should hide the list layout`() {
-//                verify(view).hideListLayout()
-//            }
-//
-//            @Test
-//            fun `should show the error layout`() {
-//                verify(view).showErrorLayout()
-//            }
-//
-//        }
-//
-//    }
-//
-//    inner class `when calling loadNextShowSet` {
-//
-//        inner class `given no more pages`() {
-//
-//            @Before
-//            fun setUp() {
-//                whenever(showRepository.findPopularShows(1)).thenReturn(singleSuccess)
-//                whenever(showData.isLastPage).thenReturn(true)
-//                presenter.bind(view)
-//                presenter.loadNextShowSet()
-//            }
-//
-//            @Test
-//            fun `should do nothing`() {
-//                verify(showRepository, never()).findPopularShows(2)
-//            }
-//
-//        }
-//
-//        inner class `given more pages to load`() {
-//
-//            @Before
-//            fun setUp() {
-//                whenever(showRepository.findPopularShows(1)).thenReturn(singleSuccess)
-//                whenever(showRepository.findPopularShows(2)).thenReturn(singleSuccess)
-//                whenever(showData.isLastPage).thenReturn(false)
-//                presenter.bind(view)
-//                presenter.loadNextShowSet()
-//            }
-//
-//            @Test
-//            fun `should fetch new page and display it`() {
-//                verify(view, times(2)).appendShows(shows)
-//            }
-//
-//        }
-//
-//    }
-//}
+
+import com.nhaarman.mockitokotlin2.*
+import com.svprdga.infinitescrollsample.domain.Show
+import com.svprdga.infinitescrollsample.domain.ShowData
+import com.svprdga.infinitescrollsample.domain.exception.KoException
+import com.svprdga.infinitescrollsample.domain.usecase.ShowsUseCase
+import com.svprdga.infinitescrollsample.presentation.presenter.view.IListView
+import com.svprdga.infinitescrollsample.util.Logger
+import de.bechte.junit.runners.context.HierarchicalContextRunner
+import io.reactivex.Single
+import io.reactivex.SingleObserver
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+@RunWith(HierarchicalContextRunner::class)
+class ListPresenterTest {
+
+    // *************************************** MOCK VARS *************************************** //
+
+    @Mock
+    lateinit var log: Logger
+    @Mock
+    lateinit var showsUseCase: ShowsUseCase
+    @Mock
+    lateinit var view: IListView
+    @Mock
+    lateinit var showData: ShowData
+
+    // ****************************************** VARS ***************************************** //
+
+    lateinit var presenter: ListPresenter
+    var shows: ArrayList<Show> = arrayListOf()
+
+    val singleSuccess = object : Single<ShowData>() {
+        override fun subscribeActual(observer: SingleObserver<in ShowData>) {
+            observer.onSuccess(showData)
+        }
+    }
+
+    val exception = KoException(400, null)
+    val singleError = object : Single<ShowData>() {
+        override fun subscribeActual(observer: SingleObserver<in ShowData>) {
+            observer.onError(exception)
+        }
+    }
+
+    // ***************************************** SET UP **************************************** //
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        for (i in 1..10) {
+            shows.add(
+                Show(
+                    i, "name_$i",
+                    "overview_$i",
+                    i.toFloat(),
+                    "path_$1",
+                    false
+                )
+            )
+        }
+
+        whenever(showData.shows).thenReturn(shows)
+
+        presenter = ListPresenter(log, showsUseCase)
+    }
+
+    // ***************************************** TESTS ***************************************** //
+
+    inner class `when calling bind()` {
+
+        @Before
+        fun setUp() {
+            whenever(showsUseCase.findPopularShows(1)).thenReturn(singleSuccess)
+        }
+
+        @Test
+        fun `should fetch initial set of items`() {
+            presenter.bind(view)
+            verify(showsUseCase).findPopularShows(1)
+        }
+
+        inner class `given success when fetching content`() {
+
+            @Test
+            fun `should display fetched items in view`() {
+                presenter.bind(view)
+                verify(view).appendShows(showData.shows)
+            }
+
+        }
+
+        inner class `given error when fetching content`() {
+
+            @Before
+            fun setUp() {
+                whenever(showsUseCase.findPopularShows(1)).thenReturn(singleError)
+                presenter.bind(view)
+            }
+
+            @Test
+            fun `should hide the list layout`() {
+                verify(view).hideListLayout()
+            }
+
+            @Test
+            fun `should show the error layout`() {
+                verify(view).showErrorLayout()
+            }
+
+        }
+
+    }
+
+    inner class `when calling loadNextShowSet` {
+
+        inner class `given no more pages`() {
+
+            @Before
+            fun setUp() {
+                whenever(showsUseCase.findPopularShows(1)).thenReturn(singleSuccess)
+                whenever(showData.isLastPage).thenReturn(true)
+                presenter.bind(view)
+                presenter.loadNextShowSet()
+            }
+
+            @Test
+            fun `should do nothing`() {
+                verify(showsUseCase, never()).findPopularShows(2)
+            }
+
+        }
+
+        inner class `given more pages to load`() {
+
+            @Before
+            fun setUp() {
+                whenever(showsUseCase.findPopularShows(1)).thenReturn(singleSuccess)
+                whenever(showsUseCase.findPopularShows(2)).thenReturn(singleSuccess)
+                whenever(showData.isLastPage).thenReturn(false)
+                presenter.bind(view)
+                presenter.loadNextShowSet()
+            }
+
+            @Test
+            fun `should fetch new page and display it`() {
+                verify(view, times(2)).appendShows(shows)
+            }
+
+        }
+
+    }
+}
